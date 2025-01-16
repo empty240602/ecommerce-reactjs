5843eadc110c9e601b7cb31e5c541a02b637ed8d
empty2406 <empty2406@gmail.com>
2025-01-16 16:56:02 +0700

    update extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import "./App.css";
+import './App.css';
+import Hello from '@components/Hello/Hello';
 
 function App() {
-  return (
-    <>
-      <div>E-commerce</div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Hello /> E-commerce
+            </div>
+        </>
+    );
 }
 
 export default App;
