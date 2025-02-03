3b8d019fbef47e1560fb1c385e5514220727c6e9
empty2406 <empty2406@gmail.com>
2025-02-03 17:05:19 +0700

    config-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css';
-import HomePage from '@components/HomePage/HomePage';
+// import HomePage from '@components/HomePage/HomePage';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+// import Blog from '@components/Blog/Blog';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
-    return (
-        <>
-            <HomePage />
-        </>
+    const root = document.getElementById('root');
+    return ReactDOM.createRoot(root).render(
+        <BrowserRouter>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {routers.map((item, index) => {
+                        return (
+                            <Route
+                                path={item.path}
+                                element={<item.component />}
+                                key={index}
+                            />
+                        );
+                    })}
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     );
 }
 
